3284afd0e9127107322461ef9a627d9a9036866a
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-08-31 14:08:47 +0100

    sixth section mobile & desktop

---
+++ components/partials/sections/home/SixthSection.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class=" w-full bg-tertiary backdrop-blur-[66px]  relative flex justify-end pt-10 lg:pt-36 ">
+            <div class="  flex lg:flex-row flex-col lg:justify-between items-end space-y-8 lg:items-start w-full max-w-[1404px] relative z-10 pl-4 2xl:pl-0 space-x-4 lg:space-x-0">
+                <div class=" max-w-[30rem] w-full flex flex-col items-start space-y-4 pt-10">
+                    <h1 class=" text-black font-epilogue text-3xl md:text-5xl font-semibold md:leading-[138%]">Organize  webinars for cobra CRM users in a blink.</h1>
+                    <p class=" text-black font-inter text-lg font-normal pb-5">
+                        Book you 20 minutes demo and
+                        <br>
+                        start planning events today.
+                    </p>
+                     <button   class="text-white font-semibold text-[0.8rem] lg:text-[1rem] lg:tracking-[0.02rem] px-4 lg:px-8 rounded-lg py-2 lg:py-3 bg-primary font-epilogue hover:bg-blue-400 hover:text-white duration-200">
+                                    Book your Demo
+                     </button>
+                </div>
+                <img src="/images/home/sixthsection/tst.png" class=" 2xl:max-w-max lg:max-w-[40rem] w-full h-full sm:w-auto object-cover sm:h-auto md:max-w-[30rem] xl:max-w-[45rem]" alt="dashboard">
+            </div>
+
+
+            <div class=" absolute top-0 left-0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="316" height="582" viewBox="0 0 316 582" fill="none">
+                    <g opacity="0.45" filter="url(#filter0_f_1_1063)">
+                        <path d="M-477.248 395.308C-423.262 488.814 -232.547 479.774 -51.2745 375.116C129.998 270.458 233.185 109.814 179.199 16.308C125.213 -77.1983 -65.5015 -68.1579 -246.774 36.5C-428.047 141.158 -531.234 301.802 -477.248 395.308Z" fill="#85AFE0"/>
+                    </g>
+                    <defs>
+                        <filter id="filter0_f_1_1063" x="-613.584" y="-170.312" width="929.12" height="752.239" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                        <feGaussianBlur stdDeviation="61" result="effect1_foregroundBlur_1_1063"/>
+                        </filter>
+                    </defs>
+                </svg>
+            </div>
+
+
+            <div class=" absolute -top-5 right-44">
+               <svg xmlns="http://www.w3.org/2000/svg" width="723" height="1163" viewBox="0 0 723 1163" fill="none">
+                <g opacity="0.45" filter="url(#filter0_f_252_1175)">
+                    <path d="M361.5 1041C493.772 1041 601 835.275 601 581.5C601 327.725 493.772 122 361.5 122C229.228 122 122 327.725 122 581.5C122 835.275 229.228 1041 361.5 1041Z" fill="#50E2C3"/>
+                </g>
+                <defs>
+                    <filter id="filter0_f_252_1175" x="0" y="0" width="723" height="1163" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                    <feGaussianBlur stdDeviation="61" result="effect1_foregroundBlur_252_1175"/>
+                    </filter>
+                </defs>
+                </svg>
+            </div>
+    </div>
+</template>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
         <ThirdSection />
         <FourthSection />
         <FifthSection />
+        <SixthSection />
     </div>
 </template>
 
@@ -18,4 +19,5 @@
  import ThirdSection from "@/components/partials/sections/home/ThirdSection.vue"
  import FourthSection from "@/components/partials/sections/home/FourthSection.vue"
  import FifthSection from "@/components/partials/sections/home/FifthSection.vue"
+ import SixthSection from "@/components/partials/sections/home/SixthSection.vue"
 </script>
