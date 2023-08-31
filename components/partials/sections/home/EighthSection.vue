9eca69a1a8b3fc91729b567953dc885bedea6a49
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-08-31 18:29:14 +0100

    fixes

--- components/partials/sections/home/EighthSection.vue
+++ components/partials/sections/home/EighthSection.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" w-full py-28 bg-white">
-        <div class=" w-full max-w-[1208px] mx-auto">
-            <div class=" w-full text-center flex flex-col items-center">
+        <div class=" w-full max-w-[1208px] mx-auto space-y-7">
+            <div class=" w-full text-center flex flex-col items-center space-y-4">
                 <span class=" text-black font-epilogue text-3xl md:text-5xl leading-[138%] font-semibold text-center">Our plans - suitable for your needs</span>
                 <span class=" text-black font-epilogue font-medium text-center text-base md:text-lg ">Sign up for annual plan now and get I month free per year!</span>
             </div>
